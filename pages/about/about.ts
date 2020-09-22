eafd82bf794e45eaf1f2b5ad3e6514ec85075781
paulmarcelo34 <marcelopaul34@gmail.com>
2020-09-22 14:15:29 +0800

    2 files modified

--- pages/about/about.ts
+++ pages/about/about.ts
@@ -3,13 +3,21 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-about',
-  templateUrl: 'about.html',
-  styleUrls: ['about.css']
+  templateUrl: 'about.html'
+  ,styleUrls: ['about.css'],
 })
 export class AboutPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+compute(){
+  this.downV=(this.Ap* .40);
+  this.LoanA=this.Ap-this.downV; 
+  
+  this.IntR=0.039;
+  this.TIP= (this.LoanA*this.IntR)*this.repay;
+  this.MonthPay=(this.LoanA+this.TIP)/this.repay;
   }
 
 }
